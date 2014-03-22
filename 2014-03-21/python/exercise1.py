2a75be2f973ae8d7d127a14522145ca0f5a0f9ea
Pietro <pie.coronas@gmail.com>
2014-03-22 02:37:08 +0100

    Primo esercizio

--- 2014-03-21/python/exercise1.py
+++ 2014-03-21/python/exercise1.py
@@ -5,8 +5,8 @@ def rgbToPlasmColor(color):
 	return [color[0]/255., color[1]/255., color[2]/255.]
 
 
-#Creazione della base. Approssimata 72x33
-base1_vertici = [ [0,0], [0,72], [33,72], [33,0] ];
+#Creazione della base. Approssimata 69x33
+base1_vertici = [ [0,0], [0,69], [33,69], [33,0] ];
 base1_num_lati = [range(1,5)] 
 base1_2D = MKPOL([base1_vertici, base1_num_lati, None])
 
@@ -17,8 +17,8 @@ floor1 = PROD([base1_2D, Q(1)])
 floor1 = COLOR(rgbToPlasmColor([210,210,210]))(floor1)
 
 
-#Creo la seconda base.Approssimata 70x32
-base2_vertici = [ [0,0], [0,71], [32,71], [32,0] ];
+#Creo la seconda base.Approssimata 68x32
+base2_vertici = [ [0,0], [0,68], [32,68], [32,0] ];
 base2_num_lati = [range(1,5)] 
 base2_2D = MKPOL([base2_vertici, base2_num_lati, None])
 
@@ -30,7 +30,7 @@ floor2 = COLOR(rgbToPlasmColor([147,147,147]))(floor2)
 
 
 #Creo la terza base su cui poggeranno le colonne: 68x30
-base3_vertici = [ [0,0], [0,70], [31,70], [31,0] ];
+base3_vertici = [ [0,0], [0,67], [31,67], [31,0] ];
 base3_num_lati = [range(1,5)] 
 base3_2D = MKPOL([base3_vertici, base3_num_lati, None])
 
@@ -42,7 +42,7 @@ floor3 = COLOR(rgbToPlasmColor([95,95,95]))(floor3)
 
 
 #Creo la la  base posizionata sotto il tetto
-base4_vertici =  [ [0,0], [0,70], [31,70], [31,0] ]; 
+base4_vertici =  [ [0,0], [0,67], [31,67], [31,0] ]; 
 base4_num_lati = [range(1,5)] 
 base4_2D = MKPOL([base4_vertici, base4_num_lati, None])
 
@@ -53,21 +53,20 @@ floor4 = PROD([base4_2D, Q(3)])
 floor4 = COLOR(rgbToPlasmColor([128,128,128]))(floor4)
 
 
-#Creo il tetto.Approssimato 70x32x5
+#Creo il tetto.Approssimato 69x33x5
 from pyplasm import*
 tetto_vertici = [ [0,0], [33,0], [16,5], ];
 tetto_num_lati = [range(1,4)] 
 tetto_2D = MKPOL([tetto_vertici, tetto_num_lati, None])
 
 #Porto in 2,5D
-floor5 = PROD([tetto_2D, Q(72)])
+floor5 = PROD([tetto_2D, Q(69)])
 
 
 #Assemblo le 3 basi e il tetto
 floor2=T([1,2,3])([0.5,0.5,1])(floor2)
 floor3=T([1,2,3])([1,1,2])(floor3)
 floor4=T([1,2,3])([1,1,15])(floor4)
-building=STRUCT([floor1,floor2,floor3])
 
 
 floor5=MAP([S1,S3,S2])(floor5)
