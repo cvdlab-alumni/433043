664b86d13c7054541e63a4f8a1bf8c7746098710
Pietro <pie.coronas@gmail.com>
2014-03-22 02:37:53 +0100

    Secondo esercizio

---
+++ 2014-03-21/python/exercise2.py
@@ -0,0 +1,149 @@
+from pyplasm import*
+
+#Creo una funzione per convertire colori RGB in Plasm
+def rgbToPlasmColor(color):
+	return [color[0]/255., color[1]/255., color[2]/255.]
+	
+
+	#Creo una funzione per la circonferenza delle colonne esterne
+def base(p): 
+ u,v = p
+ return [v*COS(u), v*SIN(u)]
+domain2D = PROD([INTERVALS(2*PI)(20), INTERVALS(1)(3)])
+
+#Creo una funzione per la circonferenza delle colonne interne
+def base2(p): 
+ u,v = p
+ return [(v/2)*COS(u), (v/2)*SIN(u)]
+
+ #Creo una colonna esterna
+base_colonna1=MAP(base)(domain2D) 
+base_colonna=PROD([base_colonna1,Q(11)])
+
+#Creo una colonna interna
+base_colonnina1=MAP(base2)(domain2D)
+base_colonnina=PROD([base_colonnina1,Q(11.5)])
+
+#Creo il blocchetto da mettere sopra la colonna esterna
+blocchetto_coords = [ [0,0], [0,2.5], [2.5,2.5], [2.5,0] ];
+blocchetto_num_lati = [range(1,5)] 
+blocchetto_2D = MKPOL([blocchetto_coords, blocchetto_num_lati, None])
+blocchetto = PROD([blocchetto_2D, Q(1)])
+
+#Posiziono il blocchetto della colonna esterna
+blocchetto=T([1,2,3])([-1.25,-1.25,11])(blocchetto)
+
+#Creo la struttura con colonna e blocchetto
+colonna=STRUCT([blocchetto,base_colonna])
+
+#Creo il blocchetto della colonna interna
+blocchetto2_coords = [ [0,0], [0,1.5], [1.5,1.5], [1.5,0] ];
+blocchetto2_num_lati = [range(1,5)] 
+blocchetto2_2D = MKPOL([blocchetto2_coords, blocchetto2_num_lati, None])
+blocchetto2 = PROD([blocchetto2_2D, Q(0.5)])
+
+#Posiziono il blocchetto sulla colonna interna
+blocchetto2=T([1,2,3])([-0.80,-0.80,11.5])(blocchetto2)
+
+#Creo la struttura con colonna interna e blocchetto
+colonna_int=STRUCT([blocchetto2,base_colonnina])
+
+#Traslo la colonna
+colonna=T([1,2,3])([2.2,2.2,3])(colonna)
+
+#Traslo la colonna interna
+colonna_int=T([1,2,3])([8.2,8.2,3])(colonna_int)
+
+#Creo le colonne frontali e le posiziono
+colonne_temp=[T(1)(4),colonna]
+colonne_frontali=STRUCT(NN(8)(colonne_temp))
+colonne_frontali=T(1)(-3.80)(colonne_frontali)
+
+#Creo le colonne interne frontali
+colonne_int_temp=[T(1)(4),colonna_int]
+colonne_int_frontali=STRUCT(NN(6)(colonne_int_temp))
+colonne_int_frontali=T(1)(-5.80)(colonne_int_frontali)
+
+
+#Creo le mura interne frontali
+mura4_vertici = [ [0,0], [1,0], [0,5], [1,5] ];
+mura4_num_lati = [range(1,5)] 
+mura4_2D = MKPOL([mura4_vertici, mura4_num_lati, None])
+mura4 = PROD([mura4_2D, Q(12)])
+
+mura4=T([1,2,3])([20,-26.5,3])(mura4)
+mura4=ROTATE([1,2])(PI/2)(mura4)
+
+
+mura5_vertici = [ [1,1], [2,1], [1,6], [2,6] ];
+mura5_num_lati = [range(1,5)] 
+mura5_2D = MKPOL([mura5_vertici, mura5_num_lati, None])
+mura5 = PROD([mura5_2D, Q(12)])
+
+mura5=T([1,2,3])([19,-12.5,3])(mura5)
+mura5=ROTATE([1,2])(PI/2)(mura5)
+
+mura_frontali=STRUCT([mura4,mura5])
+
+#Creo la struttua frontale
+
+north=STRUCT([colonne_frontali,colonne_int_frontali,mura_frontali])
+
+
+#Creo le colonne posteriori e le posiziono
+colonne_posteriori=T(2)(64)(colonne_frontali)
+
+#Creo le colonne interne posteriori
+colonne_int_posteriori=T(2)(52)(colonne_int_frontali)
+
+#Creo le mura interne posteriori
+mura3_vertici = [ [0,0], [1,0], [0,20], [1,20] ];
+mura3_num_lati = [range(1,5)] 
+mura3_2D = MKPOL([mura3_vertici, mura3_num_lati, None])
+mura3 = PROD([mura3_2D, Q(12)])
+
+mura3=T([1,2,3])([50,-26.5,3])(mura3)
+mura3=ROTATE([1,2])(PI/2)(mura3)
+
+#Creo la struttura posteriore
+
+sud=STRUCT([colonne_posteriori,colonne_int_posteriori,mura3])
+
+
+#Creo le colonne esterne sx e le posiziono
+colonne_sx=STRUCT(NN(15)(colonne_temp))
+colonne_sx=ROTATE([1,2])(PI/2)(colonne_sx)
+colonne_sx=T(1)(4.3)(colonne_sx)
+
+#Creo le mura sx e le posiziono
+mura1_vertici = [ [0,0], [1,0], [0,45], [1,45] ];
+mura1_num_lati = [range(1,5)] 
+mura1_2D = MKPOL([mura1_vertici, mura1_num_lati, None])
+mura1 = PROD([mura1_2D, Q(12)])
+
+mura1=T([1,2,3])([6.2,11.2,3])(mura1)
+
+
+#Creo la struttura sx
+ovest=STRUCT([colonne_sx,mura1])
+
+#Creo le colonne esterne dx e le posiziono
+colonne_dx=STRUCT(NN(15)(colonne_temp))
+colonne_dx=ROTATE([1,2])(PI/2)(colonne_dx)
+colonne_dx=T(1)(32.8)(colonne_dx)
+
+#Creo le mura dx e le posiziono
+mura2_vertici = [ [5,0], [6,0], [5,45], [6,45] ];
+mura2_num_lati = [range(1,5)] 
+mura2_2D = MKPOL([mura2_vertici, mura2_num_lati, None])
+mura2 = PROD([mura2_2D, Q(12)])
+
+mura2=T([1,2,3])([21,11.2,3])(mura2)
+
+#Creo la struttura sx
+est=STRUCT([colonne_dx,mura2])
+
+
+#Creo la struttura finale
+two_and_half_Verticalmodel=STRUCT([north,sud,ovest,est])
+VIEW(two_and_half_Verticalmodel)
